9e04260cd45b9531dc0c137333b5b105c6a67336
Lawrence Foley <emwav333@gmail.com>
2014-06-27 14:01:23 -0500

    Rearranged the silence threshold checking of the audio chunks

--- pydub-test/audio_splicer.py
+++ pydub-test/audio_splicer.py
@@ -4,9 +4,9 @@ import time, configparser
 from pydub import AudioSegment
 from pydub.utils import make_chunks, db_to_float
 
-splicing_resolution = 50
+splicing_resolution = 10
 silence_threshold = 430
-pause_threshold = 10
+pause_threshold = 60
 
 def average_loudness(audio):
     average_loudness = audio.rms
@@ -18,35 +18,36 @@ def main():
     audio = audio[3000:]
     chunks = make_chunks(audio, splicing_resolution)
     build = audio[:0]
-    silence_threshold = audio.rms * db_to_float(-16)
+    silence_threshold = audio.rms * db_to_float(-9)
+    print(silence_threshold)
     resets = 0
     chunk_list = []
     for i in range(len(chunks)):
 
         if chunks[i].rms < silence_threshold:
-            # if(i == 0):
-            #     chunk_list.append(build)
-            if (0 < x):
-                x -= 1
-                print("decrement x")
-            else:
+            if (0 > x):
                 x = pause_threshold
                 print("reset x")
                 resets += 1
                 chunk_list.append(build)
                 build = audio[:0]
+            else:
+                x -= 1
+                print("decrement x")
+
 
         else:
 
             build += chunks[i]
     build = audio[:0]
     print(len(chunk_list))
-    for i in range(len(chunk_list)):
+    for i in range(1, len(chunk_list)):
         print("ex")
         build = chunk_list[i]
         build.export(("test/test{0}.mp3".format(str(i))), format="mp3")
     print("resets: " + str(resets))
     build.export("test2.mp3", format="mp3")
+    print(silence_threshold)
 
 if __name__ == '__main__':
     start_time = time.time()
